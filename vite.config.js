a032842855db04c441f78a44a6f425e28952e703
Andwar22 <and.war22@gmail.com>
2026-08-09 12:43:44 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  base: '/',
+  base: '/splendid-portofolio-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
