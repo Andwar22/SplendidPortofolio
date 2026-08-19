70dd21c3e035da62077e92d73ae753e3457a233a
Andwar22 <and.war22@gmail.com>
2026-08-19 11:25:18 +0700

    setup github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/SplendidPortofolio/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
